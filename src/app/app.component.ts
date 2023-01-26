e73bd1033258ccc5e35e44cf17c0513edabbe151
Aymar <yannyao4066@gmail.com>
2023-01-26 00:16:10 +0000

    update commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { fromEvent, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'DailyChallenge';
+  @ViewChild("myDiv", { static: true }) divMy!: ElementRef;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+
+    fromEvent(this.divMy.nativeElement, 'focus')
+        .subscribe(res => console.log("Focus event"));
+
+        fromEvent(this.divMy.nativeElement, 'blur')
+        .subscribe(res => console.log("Blur event"));
+
+  }
+
+
+    
+
 }
